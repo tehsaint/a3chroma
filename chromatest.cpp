30c72ceb4a732b2b63c3eaea84e67fc167cb03bf
tehsaint <dahnoobempor@gmail.com>
2026-07-02 14:01:54 -0600

    Update chromatest.cpp

--- chromatest.cpp
+++ chromatest.cpp
@@ -1,3 +1,4 @@
 // probe to see if I can send JSON requests using c++
 
-curl -x POST "https://chromasdk.io:54236/razer/chromasdk"
+curl -x GET "https://chromasdk.io:54236/razer/chromasdk"
+curl -x POST "https://chromasdk.io:54236/razer/chromasdk" 
